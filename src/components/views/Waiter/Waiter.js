60b20793519b04048cd29a198644c9429e65c70a
Ingrid Pruszynska <pruszynskaingrid@outlook.com>
2020-12-11 17:00:38 +0100

    Waiter workflow

--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import styles from './Waiter.scss'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 const Waiter = () => {
   return (
-    <div className={styles.component}>
-      <h2>Ordering Waiter view</h2>
-      <Link to={`${process.env.PUBLIC_URL}/tables/order/new`}>New Order</Link>
-      <Link to={`${process.env.PUBLIC_URL}/tables/ordering/order/:id`}>Order Id</Link>
-    </div>
+    <Paper className={styles.component}>
+      <Typography variant="h5">WAITER WORKFLOW</Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Table</TableCell>
+              <TableCell>Order ID</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <Button variant="contained" color='secondary' component={NavLink} to={`${process.env.PUBLIC_URL}/ordering/order/`}>
+              order
+            </Button>
+          </TableBody>
+        </Table>
+    </Paper>
   )
 }
 
